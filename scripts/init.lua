19d814024cbfeba4fd5698046edb1c8db489d91c
wasquel.valentin <wasquel.valentin@gmail.com>
2026-05-30 19:25:49 +0200

    add out-of-logic indication if "require knowledge" enable

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,7 @@
 -- Items
 Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/modifiers.json")
+Tracker:AddItems("items/knowledge_elements.json")
 
 Tracker:AddMaps("maps/maps.json")
 ScriptHost:LoadScript("scripts/locations.lua")
@@ -26,3 +27,17 @@ end
 ScriptHost:AddWatchForCode("WatchGolden", "golden", updateTotal)
 ScriptHost:AddWatchForCode("WatchAnti", "anti", updateTotal)
 ScriptHost:AddWatchForCode("PreventTotal", "total", updateTotal)
+
+local function updateKnowTetro()
+    local total = Tracker:FindObjectForCode("total")
+    local throne = Tracker:FindObjectForCode("bigthrone_room")
+    local know_tetro = Tracker:FindObjectForCode("know_tetro")
+
+    if not total or not throne or not know_tetro then
+        return
+    end
+
+    know_tetro.Active = throne.Active and total.AcquiredCount >= 2
+end
+
+ScriptHost:AddOnFrameHandler("updateKnowTetro", updateKnowTetro)
